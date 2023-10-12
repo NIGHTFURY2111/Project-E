686efc4d701fc66b5ed6928818fea7823ce0577f
qutbuddin111 <113630691+qutbuddin111@users.noreply.github.com>
2023-10-12 19:20:48 +0530

    falling platform updated

--- Project E/Assets/Scripts/object script/falling object.cs
+++ Project E/Assets/Scripts/object script/falling object.cs
@@ -11,9 +11,16 @@ public class falling_object : MonoBehaviour
     [SerializeField] private LayerMask player;
     [SerializeField] private Transform rayCasts;
     [SerializeField] private float raylenght;
+    private Movement PlayerScript;
+    private void Start()
+    {
+        PlayerScript = GameObject.FindWithTag("Player").GetComponent<Movement>();
+
+    }
+   
     private void FixedUpdate()
     {  
-        RaycastHit2D hit = Physics2D.BoxCast(rayCasts.position,transform.localScale,0f,Vector2.down,raylenght,player);
+        RaycastHit2D hit = Physics2D.BoxCast(rayCasts.position,new Vector2(transform.localScale.x +3,transform.localScale.y+raylenght),0f,Vector2.down,raylenght,player);
        
        
         if (hit.collider != null/*||hit1.collider != null*/)
@@ -29,9 +36,14 @@ public class falling_object : MonoBehaviour
         rb.bodyType = RigidbodyType2D.Dynamic;
         rb.gravityScale = gravityScale; 
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerScript.Respawn();
+
+    }
     private void OnDrawGizmos()
     {
 
-        Gizmos.DrawCube(new Vector3(rayCasts.position.x,rayCasts.position.y-raylenght,rayCasts.position.z), transform.localScale);
+        Gizmos.DrawCube(new Vector3(rayCasts.position.x,rayCasts.position.y-raylenght,rayCasts.position.z), new Vector2(transform.localScale.x +3, transform.localScale.y + raylenght));
     }
 }
